6dca99717ec41ad35bd47cedbac2871eb3ec891a
Florencia Sosa <florenciassoledadsosa@gmail.com>
2021-08-26 12:33:32 -0300

    added project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { ListadoComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    ListadoComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ListadoComponent]
 })
 export class AppModule { }
